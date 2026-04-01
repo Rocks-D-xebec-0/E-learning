d18b195d8881143058fe5e7cd474c2720162d649
QA <houssemeddine.mosbah@horizon-university.tn  git config --global user.name QA  git config --global user.email houssemeddine.mosbah@horizon-university.tn>
2026-04-01 16:38:38 +0100

    initial

---
+++ src/test/java/com/JPA/exercice/ExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPA.exercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
